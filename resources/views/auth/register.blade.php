3895797caca3a14dae43bfb5f4164468c573406f
quanchun <hoangquannit@gmail.com>
2016-09-08 16:38:27 +0700

    add register form

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+  <head>
+      <title>Laravel demo</title>
+      <meta charset="utf-8">
+  </head>
+  <body>
+      <h2> Signup form :</h2>
+      <form method="POST" action={{url('/auth/register')}}>
+
+      </form>
+  </body>
+</html>
