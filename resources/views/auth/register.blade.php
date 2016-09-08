b1f3be6f6380678a5e1bc07822a0e89b7c8c9691
quanchun <hoangquannit@gmail.com>
2016-09-08 16:49:50 +0700

    updated add show error on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,14 @@
   <body>
       <h2> Signup form :</h2>
       <form method="POST" action={{url('/auth/register')}}>
-
+           {!! csrf_field() !!}}
+              @if ($errors->any())
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              @endif
       </form>
   </body>
 </html>
