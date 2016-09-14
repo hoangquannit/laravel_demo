d1b9dd2528408eaf74bcfe17936ae00f0511a35d
quanchun <hoangquannit@gmail.com>
2016-09-14 08:42:10 +0700

    add link to home on nav

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">Laravel Demo</a>
+            <a class="navbar-brand" href="{{url('/')}}">Laravel Demo</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
